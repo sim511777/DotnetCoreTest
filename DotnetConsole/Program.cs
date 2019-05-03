98032b566b12fd1d665df15c66ecf94e96498932
sim511777 <sim51177@gmail.com>
2019-05-03 19:44:05 +0900

    initial upload

---
+++ DotnetConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace DotnetConsole {
+    class Program {
+        static void Main (string[] args) {
+            foreach (var jul in Enumerable.Range(1, 9)) {
+                foreach (var dan in Enumerable.Range(2, 8)) {
+                    System.Console.Write("{0}*{1}={2,2} ", dan, jul, dan * jul);
+                }
+                System.Console.WriteLine();
+            }
+        }
+    }
+}
